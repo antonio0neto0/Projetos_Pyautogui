0cdc659087b3c607afdf41528c51c19f525e8e9b
Antonio Neto <antonio.enge200@gmail.com>
2025-02-04 15:10:53 -0300

    Desafio criar uma pasta

---
+++ clique_teste.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+# pyautogui.click(1052,425,clicks=50,interval=0.1,button='left',duration=2)
+
+# https://cpstest.org/100-seconds.php?_gl=1*kcm8wx*_ga*NDc0Mzk3NTI2LjE3MzgyNjIyMTU.*_ga_37GXT4VGQK*MTczODY4OTQwNy42LjEuMTczODY4OTc5NS4wLjAuMA..
+
+
+# Desafio criar uma nova pasta
+pyautogui.rightClick(1007,444, duration=1)
+pyautogui.move(20,-125,duration=1)
+pyautogui.click()
+pyautogui.move(-50,0,duration=1)
+pyautogui.click()
