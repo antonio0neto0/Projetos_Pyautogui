795e6ad7d5d45f33c9df1b0be84f1948aa2351d3
Antonio Neto <antonio.enge200@gmail.com>
2025-02-21 10:43:50 -0300

    Bot de WhatsApp simples

--- bot_WhatsApp.py
+++ bot_WhatsApp.py
@@ -13,8 +13,10 @@ telefones = [5513996864551]
 for telefone in telefones:
     webbrowser.open(f'https://api.whatsapp.com/send?phone={telefone}')
     sleep(10)
-    pyautogui.click(1512,208,duration=1)
+    pyautogui.click(696,360,duration=1)
+    pyautogui.click(760,223,duration=1)
     sleep(10)
     pyautogui.typewrite('Gostaria de participar do nosso evento?(digite sim, se gostaria de participar.')
     sleep(5)
     pyautogui.press('enter')
+    sleep(300)
